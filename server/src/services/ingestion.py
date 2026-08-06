48cf80b97e400864dc78fdc3b30b7c68a5206f52
sandstorm1243 <66744916+sandstorm1243@users.noreply.github.com>
2026-08-06 23:16:31 +0530

    async issue reolved, might implement sync data ingestion later

--- server/src/services/ingestion.py
+++ server/src/services/ingestion.py
@@ -1,18 +1,48 @@
-from llama_cloud import AsyncLlamaCloud
+# from llama_cloud import AsyncLlamaCloud
 
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+# client = AsyncLlamaCloud(api_key=os.getenv("LLAMA_API_KEY"))
+
+# file_obj = await client.files.create(file="D:\enterprise-financial-RAG-engine\server\data\PDF Solutions_Typeset Proxy_v2_Host.pdf", purpose="parse")
+
+# result = await client.parsing.parse(
+#     file_id=file_obj.id,
+#     tier="agentic",
+#     expand=["markdown_full"],
+# )
+
+# print(result.markdown_full)
+
+
+
+import asyncio
 import os
 from dotenv import load_dotenv
-load_dotenv()
+from llama_cloud import AsyncLlamaCloud
 
+load_dotenv()
 
 client = AsyncLlamaCloud(api_key=os.getenv("LLAMA_API_KEY"))
 
-file_obj = await client.files.create(file="D:\enterprise-financial-RAG-engine\server\data\PDF Solutions_Typeset Proxy_v2_Host.pdf", purpose="parse")
 
-result = await client.parsing.parse(
-    file_id=file_obj.id,
-    tier="agentic",
-    expand=["markdown_full"],
-)
+async def main():
+    file_obj = await client.files.create(
+        file=r"D:\enterprise-financial-RAG-engine\server\data\PDF Solutions_Typeset Proxy_v2_Host.pdf",
+        purpose="parse",
+    )
 
-print(result.markdown_full)
+    result = await client.parsing.parse(
+        file_id=file_obj.id,
+        tier="agentic",
+        expand=["markdown_full"],
+    )
+
+    print(result.markdown_full)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
